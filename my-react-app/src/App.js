14203412c21d96a8691aee7bb512bc7db09c0df2
Wub A <ayele.wub@gmail.com>
2020-07-11 23:44:30 -0400

    scaffolded react app and created jumbotron component

---
+++ my-react-app/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Jumbotron from './components/Jumbotron';
+
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <Jumbotron />
+    </div>
+  );
+}
+
+export default App;
---
+++ my-react-app/src/components/Jumbotron/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./style.css";
+
+function Jumbotron() {
+    return (
+        <div>
+            <div class="jumbotron jumbotron-fluid">
+                <div class="container">
+                    <h1 class="display-4">Employee Directory</h1>
+                    <p class="lead">Clikc on carrots to filter by heading or use the search box to narrow your results.</p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Jumbotron
