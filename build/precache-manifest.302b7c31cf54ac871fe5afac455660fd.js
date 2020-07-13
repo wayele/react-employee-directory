ef4ae8862c9dd83be981d645b73c58de2defdb2a
Wub A <ayele.wub@gmail.com>
2020-07-13 15:21:40 -0400

    build

---
+++ build/precache-manifest.302b7c31cf54ac871fe5afac455660fd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7eafa028a0518b374721cc0f31bae33c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "efa346043b13596cf004",
+    "url": "/static/css/main.0a387ecd.chunk.css"
+  },
+  {
+    "revision": "4068c12f121999d954e2",
+    "url": "/static/js/2.d033b77d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d033b77d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "efa346043b13596cf004",
+    "url": "/static/js/main.c44d0b9f.chunk.js"
+  },
+  {
+    "revision": "89ac118f8d664424e2aa",
+    "url": "/static/js/runtime-main.40fd9fda.js"
+  }
+]);
